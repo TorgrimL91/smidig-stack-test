ba948bbaa518a610b446eb71eacd61fbb7c7b30b
Anez <31077044+alasal17@users.noreply.github.com>
2019-05-24 01:00:53 +0200

    Salam
    frontend: header, dashbord og registereringsskjema
    backend: jpa annotation for entity User

---
+++ smidig-frontend/src/components/header/Dashboard.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+
+import ProjectItem from "./ProjectItem";
+
+
+
+class Dashboard extends Component {
+
+  render() {
+
+    return (
+
+      <div className="projects">
+
+        <div className="container">
+
+          <div className="row">
+
+            <div className="col-md-12">
+
+              <h1 className="display-4 text-center">User profile page</h1>
+
+              <br />
+
+
+
+              <br />
+
+              <hr />
+
+              <ProjectItem />
+
+            </div>
+
+          </div>
+
+        </div>
+
+      </div>
+
+    );
+
+  }
+
+}
+
+
+
+export default Dashboard;
